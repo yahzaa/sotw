082967fe95b07d3be916abd755b8e731f829f4f7
Chandrakant Kumar <k.03chandra@gmail.com>
2012-10-25 14:29:15 +0530

    added fb js sdk

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
-    return 'Hello World!'
+    styles_url = url_for('static', filename='style.css')
+    return render_template('root.html', styles_url=styles_url)
 
-@app.route('/hello')
-def hello():
-    return 'Hello'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
